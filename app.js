161a61721e55eeeb17cb35bb592232aa46133ef1
Micah22 <92904865+Micah22@users.noreply.github.com>
2022-03-29 18:39:54 -0500

    Have the app posting IDs successfully

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let app = express();
 
 // set up handlebars view engine
 let handlebars = require('express-handlebars')
-	.create({ defaultLayout:'main' });
+	.create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -12,25 +12,34 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(express.urlencoded({}))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.render('home');
 });
 
+app.post('/', function (req, res) {
+	res.render('ballot', {
+		voterId: req.body.voterId,
+		precinct: req.body.precinct
+	});
+});
+
+
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.status(404);
 	res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function () {
+	console.log('Express started on http://localhost:' +
+		app.get('port') + '; press Ctrl-C to terminate.');
 });
